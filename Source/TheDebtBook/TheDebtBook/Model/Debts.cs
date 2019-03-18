4247a68ae4be3f2ce84404278b6c67e1cf808254
Mie Nielsen <au581183@uni.au.dk>
2019-03-18 19:25:31 +0100

    Model donejohn

--- Source/TheDebtBook/TheDebtBook/Model/Debts.cs
+++ Source/TheDebtBook/TheDebtBook/Model/Debts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
@@ -11,18 +12,23 @@ namespace TheDebtBook.Model
     {
         public String Name { get; set; }
         public double Sum { get; set; }
-        public List<double> Amount;
+        public List<double> Amounts;
 
-        public CalculateSum(List<double> Amount)
+        public Debts(string name, double currentAmount)
+        {
+            Name = name;
+            Amounts.Add(currentAmount);
+            CalculateSum(Amounts);
+        }
+
+        public double CalculateSum(List<double> Amounts)
         {
             Sum = 0;
-            foreach (var value in Amount)
+            foreach (var value in Amounts)
             {
                 Sum =+ value;
             }
-
+            return Sum;
         }
-
-
     }
 }

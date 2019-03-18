242a47b9d4ccb4f6bc6c25516bb6ca53a3ed2cb0
Mie Nielsen <au581183@uni.au.dk>
2019-03-18 18:12:57 +0100

    Let's getting started in here

---
+++ Source/TheDebtBook/TheDebtBook/Model/Debts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDebtBook.Model
+{
+    class Debts
+    {
+        public String Name { get; set; }
+        public double Sum { get; set; }
+        public List<double> Amount;
+
+        public CalculateSum(List<double> Amount)
+        {
+            Sum = 0;
+            foreach (var value in Amount)
+            {
+                Sum =+ value;
+            }
+
+        }
+
+
+    }
+}

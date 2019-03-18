e9a626a5f529485ecbbf1f58127f2721ea6a5d71
MartinFisker <42799633+MartinFisker@users.noreply.github.com>
2019-03-18 17:33:44 +0100

    Er begyndt på ViewModel

---
+++ Source/TheDebtBook/TheDebtBook/ViewModel/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheDebtBook.Model;
+
+namespace TheDebtBook.ViewModel
+{
+    class ViewModel : ObservableCollection<Debts>
+    {
+        private ObservableCollection<Debts> debts;
+
+        
+        
+    }
+}
